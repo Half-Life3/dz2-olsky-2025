2bd7f2600d9629f17b1d218d91cf5bc98b6e3d32
Half Life 3 <olsky7@yandex.ru>
2025-01-19 17:10:53 +0300

    Done dev and test

--- Dev_Core.py
+++ Dev_Core.py
@@ -1,9 +1,13 @@
 import uuid
+from datetime import datetime
+from webbrowser import Error
+
 
 class Item:
     '''
 Item (класс предметов, хранящихся на складе)
-Item должен иметь уникальный _id, наиминование, описание, дату, в которую он должен быть отправлен а так же множество тегов _tags. Должен поддерживать добавление и удаление тегов. (В нашей задаче теги это просто строки ненулевой длинны, например "Хрупкий", "Скоропортящийся" и т.д.)
+Item должен иметь уникальный _id, наиминование, описание, дату, в которую он должен быть отправлен а так же множество тегов _tags.
+Должен поддерживать добавление и удаление тегов. (В нашей задаче теги это просто строки ненулевой длинны, например "Хрупкий", "Скоропортящийся" и т.д.)
     '''
     _id: str
     name: str
@@ -18,36 +22,39 @@ Item должен иметь уникальный _id, наиминование,
     def rm_tag(self, tag: str):
         return self._tags.remove(tag)
 
-    def __init__(self, name: str, discryption: str, dispatch_time: str, tags: str, cost = 100):
-        #Для Item убедитесь что id всё время генерируется новый
+    def __init__(self, name: str, discryption: str, dispatch_time: str, tags: str, cost=100):
+        # Для Item убедитесь что id всё время генерируется новый
         self._id = uuid.uuid4().hex
         self.name = name
         self.discryption = discryption
         self.dispatch_time = dispatch_time
-        self._tags = set(tags.split(","))
+        self._tags = set(tags.split(", "))
         self._cost = cost
 
     def __str__(self):
-        #Реализацию __str__ продумайте сами, учитывая то что вам может потребоваться знать про эти объекты для дальнейшей работы с ними
+        # Реализацию __str__ продумайте сами, учитывая то что вам может потребоваться знать про эти объекты для дальнейшей работы с ними
         return self.name
 
     def __repr__(self):
-        #Для Item в __repr__ в строку включите первые 3 (или меньше) тега из _tags и его id
+        # Для Item в __repr__ в строку включите первые 3 (или меньше) тега из _tags и его id
         if len(self._tags) >= 3:
-            return f"Item: {self._id} with tags:{self._tags[:3]}"
+            return f"Item: {self._id} with tags:{list(self._tags)[:3]}"
         else:
-            return f"Item: {self._id} with tags:{self._tags[:len(self._tags)]}"
+            return f"Item: {self._id} with tags:{list(self._tags)[:len(self._tags)]}"
 
     def __len__(self) -> int:
-        #Для Item функция len(item) должна возвращать количество тегов этого предмета
+        # Для Item функция len(item) должна возвращать количество тегов этого предмета
         return len(self._tags)
 
-    def set_cost(self, cost: int):
-        self._cost = cost
-        return f'Значение cost изменено на {cost}'
-
-    def get_cost(self) -> int:
+    @property
+    def cost(self):
         return self._cost
+
+    @cost.setter
+    def cost(self, value):
+        if value < 0:
+            raise ValueError('Cost cannot be negative')
+        self._cost = value
 
     def __lt__(self, other):
         """Сравнивает товары по количесву*цена"""
@@ -64,12 +71,29 @@ Item должен иметь уникальный _id, наиминование,
 
     def __getitem__(self, position):
         sorted_item = sorted(self.stacks, key=lambda x: self.stacks[position].item.cost)
-        return f"{position + 1} is the {sorted_stacks[position]}"
+        return f"{position + 1} is the {sorted_item[position]}"
+
+    def add_tags(self, tags: str):
+        tags = list(tags.split(", "))
+        for tag in tags:
+            self._tags.add(tag)
+
+    def rm_tags(self, tags: str):
+        tags = list(tags.split(", "))
+        for tag in tags:
+            self._tags.remove(tag)
+
+    def copy(self):
+        '''метод copy(self), который возвращает новый item с таким же описанием, ценой и именем, но с другим id'''
+        new_item = Item(self.name, self.discryption, self.dispatch_time, str(self._tags), self._cost)
+        return new_item
+
 
 class Hub:
     """
 Hub (синглтон, класс объекта нашего склада)
-Hub должен поддерживать обращение к предметам по индексам и иметь метод добавления предмета в лист _items, поле _date с датой в любом формате, а так же быть синглтоном (при любом вызове Hub() возвращается один и тот же инстантс объекта)
+Hub должен поддерживать обращение к предметам по индексам и иметь метод добавления предмета в лист _items,
+поле _date с датой в любом формате, а так же быть синглтоном (при любом вызове Hub() возвращается один и тот же инстантс объекта)
     """
     _items: list[Item]
     _date: str
@@ -77,8 +101,11 @@ Hub должен поддерживать обращение к предмета
     _hub_instance = None
 
     def add_item(self, item):
-        return self._items.append(item)
-    # [i] -> item
+        # Измените метод add_item(item) так чтобы он проверял что item действительно является типом Item или его наследником
+        if isinstance(item, Item):
+            self._items.append(item)
+        else:
+            raise TypeError("item должен быть экземпляром класса Item или его наследником")
 
     def __new__(cls, *args, **kwargs):
         # Сделать синглтон
@@ -91,17 +118,16 @@ Hub должен поддерживать обращение к предмета
         self._date = date
         self.hub_name = hub_name
 
-
     def __str__(self):
         # Реализацию __str__ продумайте сами, учитывая то что вам может потребоваться знать про эти объекты для дальнейшей работы с ними
         return self.hub_name
 
     def __repr__(self):
-        #Для Hub в __repr__ в строку включите первые 3 (или меньше) предмета из _items
+        # Для Hub в __repr__ в строку включите первые 3 (или меньше) предмета из _items
         if len(self._items) >= 3:
             return f"Hub: {self.hub_name} with items:{self._items[0:3]}"
-        else: f"Hub: {self.hub_name} with items:{self._items[0:len(self._items)]}"
-
+        else:
+            f"Hub: {self.hub_name} with items:{self._items[0:len(self._items)]}"
 
     def __len__(self):
         # Для Hub функция len(hub) должна возвращать количество предметов на данный момент
@@ -110,7 +136,8 @@ Hub должен поддерживать обращение к предмета
     def __getitem__(self, position):
         if 0 <= position < len(self._items):
             return self._items[position]
-        else: raise IndexError('Бу! Испугался? Ты не бойся')
+        else:
+            raise IndexError('Бу! Испугался? Ты не бойся')
 
     def find_by_id(self, item_id) -> str:
         for item in self._items:
@@ -123,7 +150,78 @@ Hub должен поддерживать обращение к предмета
         (для этого будет полезно сделать метод в Items is_tagged(tag) и/или реализуйте в нём итератор по тегам, чтобы не лезть в протектед поле _tags)'''
         result_list = []
         for item in self._items:
-            for i in range(len(item._tags)):
-                if item.is_tagged(tags[i]):
+            for i in range(len(tags)):
+                if item.is_tagged(tags[i]) and i == len(tags) - 1:
                     result_list.append(item)
+                elif item.is_tagged(tags[i]):
+                    continue
+                else:
+                    break
         return result_list
+
+    def rm_item(self, i):
+        '''Реализуйте метод rm_item(i) который удаляет item с id=i если i это число, или удаляет item=i если i это Item.'''
+        if type(i) == str:
+            for item in self._items:
+                if item.get_id() == i:
+                    self._items.remove(item)
+        elif type(i) == Item:
+            self._items.remove(i)
+        else:
+            raise TypeError('Неверный тип данных для удаления итема')
+
+    def drop_items(self, items: list):
+        """Так же полезным будет метод drop_items(items), который уберает вообще все товары из Hub, которые содержатся в items"""
+        for item in items:
+            if item in self._items:
+                self._items.remove(item)
+
+    def clear(self):
+        """Из утилит так же может быть полезен clear() который полностью отчистит весь контейнер _items"""
+        items = self._items.copy()
+        for item in items:
+            self._items.remove(item)
+        if len(self) != 0:
+            raise Exception("That's was an error")
+
+    @property
+    def date(self):
+        """Реализуйте метод set_date() и get_date() (или реализуйте поле _date через property) для установки даты в хабе"""
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        date_obj = datetime.strptime(date, "%d.%m.%Y")
+        if date_obj.year < 2000:
+            raise ValueError('Дата раньше 2000 года')
+        self._date = date
+
+    def find_by_date(self, date1, date2=None):
+        """Реализуйте метод find_by_date(), который возвращает лист всех Item, подходящих по дате. Реализуйте его таким образом,
+        что если в метод передаётся только одна дата, то возвращаются все items с датой раньше или равной ей, а если передаётся две даты,
+        тогда все items с датой в этом промежутке (для этого можно использовать *args и смотреть сколько аргументов в явном виде передали).
+        В случае если передали слишком много параметров - бросайте ошибку"""
+        result_items = []
+        date1_obj = datetime.strptime(date1, "%d.%m.%Y")
+        if date2:
+            date2_obj = datetime.strptime(date2, "%d.%m.%Y")
+            for item in self._items:
+                date_check_obj = datetime.strptime(item.dispatch_time, "%d.%m.%Y")
+                if date1_obj <= date_check_obj <= date2_obj:
+                    result_items.append(item)
+        else:
+            for item in self._items:
+                date_check_obj = datetime.strptime(item.dispatch_time, "%d.%m.%Y")
+                if date1_obj >= date_check_obj:
+                    result_items.append(item)
+
+        return result_items
+
+    def find_most_valuable(self, amount = 1):
+        """Добавьте метод find_most_valuable(amount=1) который вернёт первые amount самых дорогих предметов на складе.
+        Если предметов на складе меньше чем amount - верните их все"""
+        sorted_items = sorted(self._items, key=lambda item: item.cost, reverse=True)
+        if amount < len(self._items):
+            return sorted_items[:amount]
+        else:
+            return sorted_items
--- Test_Core.py
+++ Test_Core.py
@@ -1,4 +1,6 @@
 import unittest
+from pprint import pprint
+
 from Dev_Core import Item, Hub
 
 
@@ -7,8 +9,8 @@ class TestItem(unittest.TestCase):
         '''Проверка того что у разных Items разные id'''
         item1 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный, сладкий")
         item2 = Item('tovar2', 'opisanie tovara2', '02012025', 'кислый, твердый, вкусный')
-        self.assertNotEquals(item1._id, item2._id)
-        #pass # Реализуйте проверку того что у разных Items разные id
+        self.assertNotEqual(item1._id, item2._id)
+        # pass # Реализуйте проверку того что у разных Items разные id
 
     def test_len(self):
         '''Проверка того что при добавлении тэгов меняется значение len(item)'''
@@ -16,11 +18,11 @@ class TestItem(unittest.TestCase):
         item3.add_tag('дорого')
         self.assertEqual(len(item3), 4)
 
-        #pass # Реализуйте проверку того что при добавлении тэгов меняется значение len(item)
+        # pass # Реализуйте проверку того что при добавлении тэгов меняется значение len(item)
 
     def test_equal_tags(self):
         '''Проверка того что если к предмету добавить два идентичных тега - их колчество будет один'''
-        pass # Реализуйте проверку того что если к предмету добавить два идентичных тега - их колчество будет один
+        pass  # Реализуйте проверку того что если к предмету добавить два идентичных тега - их колчество будет один
         item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный, сладкий")
         item3.add_tag('белый')
         self.assertEqual(len(item3), 3)
@@ -32,7 +34,45 @@ class TestItem(unittest.TestCase):
 
     def test_is_tagged(self):
         item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный, сладкий")
-        self.assertTrue(item3.is_tagged)
+        self.assertTrue(item3.is_tagged('вкусный'))
+
+    def test_repr(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "вкусный")
+        self.assertEqual(str(item3.__repr__()), f"Item: {item3._id} with tags:['вкусный']")
+
+    def test_get_cost(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный")
+        self.assertEqual(item3.cost, 100)
+
+    def test_set_cost_positive(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный")
+        item3.cost = 101
+        self.assertEqual(item3.cost, 101)
+
+    def test_set_cost_negative(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный")
+        with self.assertRaises(ValueError) as context:
+            item3.cost = -300
+        self.assertEqual(str(context.exception), "Cost cannot be negative")
+
+    def test_add_tags(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный")
+        item3.add_tags('tag1, tag2, белый')
+        self.assertEqual(item3._tags, {'tag1', 'tag2', 'белый', 'вкусный'})
+
+    def test_rm_tags(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный")
+        item3.rm_tags('белый')
+        self.assertEqual(item3._tags, {'вкусный'})
+
+    def test_copy(self):
+        item3 = Item('товар1', 'описание товара1', "01.01.2025", "белый, вкусный")
+        item3_copy = item3.copy()
+        self.assertEqual(item3.name, item3_copy.name)
+        self.assertEqual(item3.discryption, item3_copy.discryption)
+        self.assertEqual(item3.dispatch_time, item3_copy.dispatch_time)
+        self.assertEqual(item3.cost, item3_copy.cost)
+
 
 class TestHub(unittest.TestCase):
     def test_hub_singleton(self):
@@ -44,7 +84,7 @@ class TestHub(unittest.TestCase):
     def test_len(self):
         '''Проверка того что при добавлении предметов меняется значение len(item)'''
         hub1 = Hub('01.01.2025', 'Store$1')
-        item1 = Item('Tovar1', 'Descryption1', '01.01.2025','tag1, tag2, tag3')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2, tag3')
         item2 = Item('Tovar2', 'Descryption2', '01.01.2025', 'tag1, tag2, tag3, tag4')
         hub1.add_item(item1)
         hub1.add_item(item2)
@@ -83,22 +123,158 @@ class TestHub(unittest.TestCase):
         hub1.add_item(item2)
         self.assertEqual(hub1.find_by_id('123'), '-1')
 
-    def test_find_by_tags(self):
+    def test_find_by_tags_hard(self):
         hub1 = Hub('01.01.2025', 'Store$1')
         item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2, tag3')
         item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag1, tag2, tag3, tag4')
-        item3 = Item('Tovar3', 'Descryption3', '03.01.2025', 'tag5, tag6, tag7, tag8')
-        item4 = Item('Tovar4', 'Descryption4', '04.01.2025', 'tag1, tag9, tag10, tag11')
+        item3 = Item('Tovar3', 'Descryption3', '03.01.2025', 'tag5, tag6, tag7, tag2')
+        item4 = Item('Tovar4', 'Descryption4', '04.01.2025', 'tag1, tag9, tag2, tag11')
         hub1.add_item(item1)
         hub1.add_item(item2)
         hub1.add_item(item3)
         hub1.add_item(item4)
-        self.assertEqual(hub1.find_by_tags(['tag1', 'tag2']), 'qwe')
+        result_list = [item1, item2, item4]
+        self.assertEqual(hub1.find_by_tags(['tag1', 'tag2']), result_list)
+
+    def test_find_by_tags_easy(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        result_list = [item1]
+        self.assertEqual(hub1.find_by_tags(['tag1', 'tag2']), result_list)
+
+    def test_rm_item_type_item(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.rm_item(item1._id)
+        self.assertEqual(hub1.__len__(), 1)
+
+    def test_rm_item_type_id(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.rm_item(item1)
+        self.assertEqual(hub1.__len__(), 1)
+
+    def test_drop_items(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        item3 = Item('Tovar3', 'Descryption3', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.add_item(item3)
+        hub1.drop_items([item1, item2])
+        self.assertEqual(len(hub1), 1)
+
+    def test_clear(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.clear()
+        self.assertEqual(len(hub1), 0)
+
+    def test_get_date(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        self.assertEqual(hub1.date, hub1._date)
+
+    def test_set_date_positive(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.date = '02.02.2022'
+        self.assertEqual(hub1.date, '02.02.2022')
+
+    def test_set_date_negative(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        with self.assertRaises(ValueError) as context:
+            hub1.date = '02.02.1122'
+        self.assertEqual(str(context.exception), "Дата раньше 2000 года")
+
+    def test_find_by_date_1arg(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        item3 = Item('Tovar3', 'Descryption2', '03.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.add_item(item3)
+        self.assertEqual(hub1.find_by_date('02.01.2025'), [item1, item2])
+
+    def test_find_by_date_2args(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        item3 = Item('Tovar3', 'Descryption2', '03.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.add_item(item3)
+        self.assertEqual(hub1.find_by_date('02.01.2025', '03.01.2025'), [item2, item3])
+
+    def test_add_item(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        item3 = Item('Tovar3', 'Descryption2', '03.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.add_item(item3)
+        self.assertEqual(len(hub1), 3)
+
+    def test_add_item_negative(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        with self.assertRaises(TypeError) as context:
+            hub1.add_item(123)
+        self.assertEqual(str(context.exception), "item должен быть экземпляром класса Item или его наследником")
+
+    def test_most_valuable_positive(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        item3 = Item('Tovar3', 'Descryption2', '03.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.add_item(item3)
+        item1.cost = 100
+        item2.cost = 102
+        item3.cost = 101
+        self.assertEqual(hub1.find_most_valuable(), [item2])
+
+    def test_most_valuable_negative(self):
+        hub1 = Hub('01.01.2025', 'Store$1')
+        item1 = Item('Tovar1', 'Descryption1', '01.01.2025', 'tag1, tag2')
+        item2 = Item('Tovar2', 'Descryption2', '02.01.2025', 'tag2, tag3')
+        item3 = Item('Tovar3', 'Descryption2', '03.01.2025', 'tag2, tag3')
+        hub1.add_item(item1)
+        hub1.add_item(item2)
+        hub1.add_item(item3)
+        item1.cost = 100
+        item2.cost = 102
+        item3.cost = 101
+        result_items = hub1.find_most_valuable(4)
+        self.assertEqual(result_items[0], item2)
+        self.assertEqual(result_items[1], item3)
+        self.assertEqual(result_items[2], item1)
 
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
-
-
-
-
